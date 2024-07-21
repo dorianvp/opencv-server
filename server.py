f5d08438e728cec3209de376baac4a18c6a94d9e
dariovp <dariovp01@gmail.com>
2024-07-21 20:05:59 -0300

    Add word detection under pointer

--- server.py
+++ server.py
@@ -2,20 +2,28 @@ import cv2
 import socket
 import pickle
 import struct
+import Linguist
+from OCR import ocr_from_frame, OCR
+import warnings
+warnings.filterwarnings("ignore")
 
 # This is the actual server
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("0.0.0.0", 8001))
 server_socket.listen(10)
 
-# Create a socket client
-# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client_socket.connect(("0.0.0.0", 8001))
-
 received_data = b""
 payload_size = struct.calcsize("L")
 
 client_socket, address = server_socket.accept()
+
+# OCR initialization
+language = "eng"
+ocr = OCR().start()
+ocr.set_language(language)
+lang_name = Linguist.language_string(language)
+
+
 while True:
 	# Receive and assemble the data until the payload size is reached
 	while len(received_data) < payload_size:
@@ -35,7 +43,9 @@ while True:
 	received_data = received_data[msg_size:]
 
 	# Deserialize the frame
-	frame = pickle.loads(frame_data)
+	frame = pickle.loads(frame_data);
+	ocr.set_exchange(frame)
+	frame = ocr_from_frame(frame, ocr)
 
 	# Display the frame
 	cv2.imshow("from server", frame)
